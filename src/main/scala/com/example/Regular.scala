bf52bf9b5855bfa3a3c8f16b690eb103b8c7fe20
Jakub Kozłowski <kubukoz@gmail.com>
2021-03-09 02:25:41 +0100

    Implement... something

--- src/main/scala/com/example/Regular.scala
+++ src/main/scala/com/example/Regular.scala
@@ -1,5 +1,8 @@
 package com.example
 
+import scala.annotation.tailrec
+import cats.data.Chain
+
 sealed trait Regular extends Product with Serializable {
   def |(that: Regular): Regular = Regular.Alternative(this, that)
   def ~(that: Regular): Regular = Regular.Sequence(this, that)
@@ -17,13 +20,70 @@ object Regular {
 
   def apply(c: scala.Char): Regular = Char(c)
 
-  def interpret(reg: Regular, s: String): Boolean = ???
+  val interpret: (Regular, String) => Boolean = {
+    case class Result(matches: Boolean, leftover: String)
+
+    def go(current: Regular, remainder: String): Result = {
+      val noMatch = Result(false, remainder)
+
+      current match {
+        case Char(v) =>
+          remainder.headOption match {
+            case Some(next) => Result(v == next, remainder.tail)
+            case None       => noMatch
+          }
+
+        case Alternative(left, right) =>
+          val leftResult = go(left, remainder)
+
+          if (leftResult.matches)
+            leftResult
+          else
+            go(right, remainder)
+
+        case Sequence(left, right) =>
+          val leftResult = go(left, remainder)
+
+          if (leftResult.matches)
+            go(right, leftResult.leftover)
+          else
+            leftResult
+
+        case Optional(underlying) =>
+          val result = go(underlying, remainder)
+
+          if (result.matches)
+            result
+          else
+            Result(true, remainder)
+
+        case Repeated(underlying) =>
+          @tailrec
+          def loop(previous: Result): Result = {
+            val result = go(underlying, previous.leftover)
+
+            if (result.matches)
+              loop(result)
+            else
+              previous
+          }
+
+          loop(noMatch)
+      }
+    }
+
+    go(_, _).matches
+  }
+
 }
 
 object Demo extends App {
-  println(
-    ((Regular('a') ~ Regular('b') ~ Regular('C')) |
-      (Regular('d') ~ Regular('e').+) |
-      (Regular('f') ~ Regular('g'))).apply("abC|(de+)|fg")
-  )
+  // println(
+  //   ((Regular('a') ~ Regular('b') ~ Regular('C')) |
+  //     (Regular('d') ~ Regular('e').+) |
+  //     (Regular('f') ~ Regular('g'))).apply("abC|(de+)|fg")
+  // )
+  println(Regular('a').apply("a"))
+  println((Regular('c') | Regular('a') ~ Regular('d')).apply("ad"))
+  println((Regular('c') | Regular('a') ~ Regular('d')).apply("cd"))
 }
